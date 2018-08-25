53c71557600d7be6c4ea17f042d41786ec07d407
Ahmad Arif <ahmad.arif019@gmail.com>
2018-08-25 18:11:24 +0700

    final code

--- src/com/ahmadarif/thesis/Main.java
+++ src/com/ahmadarif/thesis/Main.java
@@ -8,11 +8,11 @@ import com.ahmadarif.thesis.algorithm.angle.Point3;
 
 public class Main {
 
-    static double[] y = {101,87,95,97,109,118,122,115,128,127,125,128};
+//    static double[] y = {101,87,95,97,109,118,122,115,128,127,125,128};
 //    static double[] y = {154,156,154,155,161,155,150,152,154};
 //    static double[] y = {103,131,102,130,128,74,66,117,120,118,119,122,120,117,119,120,122,121,126,126,123,127,125,121,122,122,118,114,108,114,115,110,110,111,116,122,113,117,113};
 //    static double[] y = {101,99,47,113,108,114,97,98,94,83,99,99,98,96,95,105,107,92,87,95,102,115,98,102,112,98,89,99,29,21,82};
-//    static double[] y = {116,109,97,98,102,97,103,97,95,102,104,99,109,117,122,113,113,106,110,111,103,106,111,112,113,110,104,97,112,111,113};
+    static double[] y = {116,109,97,98,102,97,103,97,95,102,104,99,109,117,122,113,113,106,110,111,103,106,111,112,113,110,104,97,112,111,113};
 
     static double[][] y2 = {
         {101,87,95,97,109,118,122,115,128,127,125,128},
@@ -45,22 +45,35 @@ public class Main {
     }
 
     static void hitungAkurasi() {
+        System.out.println("actual :=============================================================================");
+        println(y);
+
+        double alpha = 0.9;
+        double R = 0.1;
+
         // eksponensial
         ExponentialSmoothing es = new ExponentialSmoothing(y);
 
-        es.SES(0.99, true);
-        double f[] = es.SESF(0.99);
-
-//        es.DESF(0.2);
-//        double[] f = es.DESF(0.2);
-
-
-        // kalman
-        KalmanFilter kf = new KalmanFilter(y);
-//        double f[] = kf.processF(0.8);
-
+        // single eksponensial smoothing
+        System.out.println("single eksponensial smoothing :=============================================================");
+//        es.SES(alpha, true);
+        double f[] = es.SESF(alpha);
+        printAcc(y, f, false);
         println(f);
-        System.out.printf("\nAkurasi = %.2f\n", Accuration.persen(y, f, true));
+
+        // double eksponensial smoothing
+        System.out.println("double eksponensial smoothing :=============================================================");
+//        es.DES(alpha, true);
+        f = es.DESF(alpha);
+        printAcc(y, f, false);
+        println(f);
+
+        // kalman filter
+        System.out.println("kalman filter :=============================================================================");
+        KalmanFilter kf = new KalmanFilter(y);
+        f = kf.processF(R);
+        printAcc(y, f, false);
+        println(f);
     }
 
     static void optimasiEksponensial() {
@@ -72,11 +85,7 @@ public class Main {
                 double[] f = es.SESF(alpha);
                 alpha += 0.1;
 
-                System.out.printf("MAD = %.2f", Accuration.MAD(y2[index], f));
-                System.out.printf(", MSE = %.2f", Accuration.MSE(y2[index], f));
-                System.out.printf(", RMSE = %.2f", Accuration.RMSE(y2[index], f));
-                System.out.printf(", MAPE = %.2f", Accuration.MAPE(y2[index], f));
-                System.out.printf(", ACC = %.2f\n", Accuration.persen(y2[index], f, false));
+                printAcc(y2[index], f, false);
             }
             System.out.println();
         }
@@ -91,11 +100,7 @@ public class Main {
                 double[] f = kf.processF(alpha);
                 alpha += 0.1;
 
-                System.out.printf("MAD = %.4f", Accuration.MAD(y2[index], f));
-                System.out.printf(", MSE = %.4f", Accuration.MSE(y2[index], f));
-                System.out.printf(", RMSE = %.4f", Accuration.RMSE(y2[index], f));
-                System.out.printf(", MAPE = %.2f", Accuration.MAPE(y2[index], f));
-                System.out.printf(", ACC = %.2f\n", Accuration.persen(y2[index], f, false));
+                printAcc(y2[index], f, false);
             }
             System.out.println();
         }
@@ -113,10 +118,26 @@ public class Main {
         }
     }
 
+    static void printAcc(double[] Y, double[] F, boolean withText) {
+        if (withText) {
+            System.out.printf("MAD = %.2f", Accuration.MAD(Y, F));
+            System.out.printf(", MSE = %.2f", Accuration.MSE(Y, F));
+            System.out.printf(", RMSE = %.2f", Accuration.RMSE(Y, F));
+            System.out.printf(", MAPE = %.2f", Accuration.MAPE(Y, F));
+            System.out.printf(", ACC = %.2f\n", Accuration.persen(Y, F, false));
+        } else {
+            System.out.printf("%.2f\t", Accuration.MAD(Y, F));
+            System.out.printf("%.2f\t", Accuration.MSE(Y, F));
+            System.out.printf("%.2f\t", Accuration.RMSE(Y, F));
+            System.out.printf("%.2f\t", Accuration.MAPE(Y, F));
+            System.out.printf("%.2f\n", Accuration.persen(Y, F, false));
+        }
+    }
+
     public static void main(String[] args) {
-//        hitungAkurasi();
 //        optimasiEksponensial();
-        optimasiKalman();
+//        optimasiKalman();
+        hitungAkurasi();
     }
 
 }
